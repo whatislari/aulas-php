9c32e79fc003776b6b7e559e1bb507113a394976
Laricia Rayssa <larissa2maximo@gmail.com>
2024-02-20 13:46:07 -0300

    up

---
+++ aulas-php2/t14_enviar/aula08-Desafio_de_investimento/tabeladeinvestimento.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <script src="js/bootstrap.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Investimentos</h1>
+            </div>
+            <hr>
+            <form action="tabeladeinvestimento.php" class="form-control" method="post">
+
+            </form>
+
+        </div>
+    </div>
+
+
+
+
+</body>
+</html>
